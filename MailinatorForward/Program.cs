d4ce8f2a02205251af7675594dd364fa273dda0b
SEAN-LAPTOP\Sean <s.dunlop@socyinc.com>
2018-05-08 09:30:07 -0400

    Add project files.

---
+++ MailinatorForward/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Mail;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+namespace MailinatorForward
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
